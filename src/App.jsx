75b26bb02ab441a0c811d15617bc15a70efbd99e
deiji <andrewzakhari5@gmail.com>
2023-05-24 18:40:46 +0300

    done with animtaion for desktop devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import DisplaySection from './components/DisplaySection';
+import Jumbotron from './components/Jumbotron';
+import Nav from './components/Nav';
+import SoundSection from './components/SoundSection';
+import WebgiViewer from './components/WebgiViewer';
+import { useRef } from 'react';
+
 
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  }
 
   return (
     <div className="App">
-      Hello
+      <div ref={contentRef} id="parent">
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef}/>
     </div>
   );
 }
